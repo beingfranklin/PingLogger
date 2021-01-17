4a6e7ab5b3ec7b50033f778fb53971e998b896fc
Franklin Antony <franklinanton.007@gmail.com>
2021-01-17 21:01:42 +0000

    Update ping.sh

--- ping.sh
+++ ping.sh
@@ -3,13 +3,12 @@
 ping -c1 172.217.169.78 >/dev/null
 echo "Status,Pinging to,Time" >>log.csv
 while true; do
+    now=$(date +"%T")
     if [ $? -eq 0 ]; then
-        now=$(date +"%T")
         echo "ok,google.com,$now" >>log.csv
         # exit 0
     else
         echo "fail"
-        now=$(date +"%T")
         echo "fail,google.com,$now" >>log.csv
     fi
     sleep 5
